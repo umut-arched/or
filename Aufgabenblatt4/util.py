71461d07f51f9bf6cce480abb1baf683671aba8b
Tilman Gräf <tilman.graef@gmail.com>
2023-06-06 16:27:12 +0200

    Refactor name

--- Aufgabenblatt4/util.py
+++ Aufgabenblatt4/util.py
@@ -1,9 +1,10 @@
 """
 To use in other scripts:
-from read_instance.py import read_instance_from_file
+from util.py import read_instance_from_file
 """
 
 import sys
+import math
 
 def read_instance_from_file(path):
     """
@@ -21,6 +22,15 @@ def read_instance_from_file(path):
     return x_coords, y_coords
 
 
+def euclidean_distance(x1, x2, y1, y2):
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+
+def calculate_distances(x_coords, y_coords):
+    pass
+
+
+
 if __name__ == "__main__":
     """
     The following code is only used for testing. The algorithm scripts only access the read_instance_from_file function.
@@ -28,16 +38,22 @@ if __name__ == "__main__":
     However, the script can be called in order to read instances and have the lists containing x- and y-coordinates
     printed:
     
-    Usage: python read_instance.py <path to instance>
+    Usage: python util.py <path to instance>
     
-    Example: python read_instance.py /data/eins.txt
+    Example: python util.py /data/eins.txt
     """
 
     args = sys.argv
-    if len(args) != 2:
-        ValueError("Usage: python read_instance.py <path to instance>")
 
-    x_coords, y_coords = read_instance_from_file(sys.argv[1])
+    if len(args) == 1:
+        # default
+        path = "/data/eins.txt"
+    elif len(args) == 2:
+        path = sys.argv[1]
+    else:
+        ValueError("Usage: python util.py <path to instance>")
+
+    x_coords, y_coords = read_instance_from_file(path)
 
     print(x_coords)
     print(y_coords)
