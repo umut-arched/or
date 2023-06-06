a7d40d9efeb79cd9e5aa529f55863274fb6849a9
Tilman Gräf <tilman.graef@gmail.com>
2023-06-06 15:58:22 +0200

    Add function to read instances

--- Aufgabenblatt4/read_instance.py
+++ Aufgabenblatt4/read_instance.py
@@ -1,8 +1,39 @@
 
 import sys
 
-if __name__ == "__main__":
+def read_instance_from_file(path):
+    """
+    Reads coordinates from an instance
+    :param path: path to instance text file containing the coordinates
+    :return: x_coords and y_coords list where x_coords[0] is the x-coordinate of airport with index 1 in the file
+    """
+    x_coords = []
+    y_coords = []
+    with open(path, 'r') as file:
+        for line in file:
+            line = line.replace("\n", "").split(" ")
+            x_coords.append(float(line[1]))
+            y_coords.append(float(line[2]))
+    return x_coords, y_coords
 
-    args = sys.args
+
+if __name__ == "__main__":
+    """
+    The following code is only used for testing. The algorithm scripts only access the read_instance_from_file function.
+    
+    However, the script can be called in order to read instances and have the lists containing x- and y-coordinates
+    printed:
+    
+    Usage: python read_instance.py <path to instance>
+    
+    Example: python read_instance.py /data/eins.txt
+    """
+
+    args = sys.argv
     if len(args) != 2:
-        print("shit")
+        ValueError("Usage: python read_instance.py <path to instance>")
+
+    x_coords, y_coords = read_instance_from_file(sys.argv[1])
+
+    print(x_coords)
+    print(y_coords)
