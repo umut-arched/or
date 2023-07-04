36d7a3095501ff2426e22829afcc8e92c15a2970
tigr <tigr@mail.uni-paderborn.de>
2023-07-04 19:05:03 +0200

    implement greedy_knapsack_continuous.py

---
+++ Aufgabenblatt6/greedy_knapsack_continuous.py
@@ -0,0 +1,82 @@
+"""
+Usage: python greedy_knapsack_continuous.py <path to instance>
+Try:   python greedy_knapsack_continuous.py ./instances/kp_0_1_10_50.txt
+"""
+
+import sys
+from util import *
+
+
+def greedy_knapsack(items, capacity):
+
+    # sort items by profitability
+    sorted_items = sorted(items, key=lambda item: item.profitability, reverse=True)
+
+    # init objective values
+    used_capacity = 0
+    total_value = 0
+    items_in_knapsack = []
+
+    # iterate over all items
+    for i, item in enumerate(sorted_items):
+        used_capacity += item.weight
+        # check if capacity is exceeded
+        if used_capacity >= capacity:
+
+            # get percentage of last item that would fit
+            exceeding_capacity = used_capacity - capacity
+            item_percentage = 1 - (1 / item.weight * exceeding_capacity)
+
+            # catch edge cases
+            if item_percentage == 1:
+                # just add the last item if it fits completely
+                items_in_knapsack.append(item)
+                total_value += item.value
+                break
+            elif item_percentage == 0:
+                # don't add another item if the knapsack is at full capacity
+                break
+            else:
+                # create partial item
+                partial_item = Item(f"{item.id} ({item_percentage*100}%)", item.weight*item_percentage,
+                                    item.value*item_percentage)
+
+                # add new partial item to knapsack and its value to the total value
+                items_in_knapsack.append(partial_item)
+                total_value += partial_item.value
+                # set the used_capacity = capacity since it's fully used up
+                used_capacity = capacity
+
+            # break loop
+            break
+
+        items_in_knapsack.append(item)
+        total_value += item.value
+
+    return items_in_knapsack, total_value, used_capacity
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        raise ValueError("\n\tUsage: python greedy_knapsack_continuous.py <path to instance>\n"
+                         "\tTry:   python greedy_knapsack_continuous.py ./instances/kp_0_1_10_50.txt")
+    else:
+        instance_path = sys.argv[1]
+
+    num_items, weights, values, capacity = read_instance_from_file(instance_path, print_instance=True)
+    items = create_item_classes(weights, values)
+
+    items_in_knapsack, total_value, used_capacity = greedy_knapsack(items, capacity)
+
+    print("\nGreedy knapsack (continuous) result:")
+    print(f"  Total value:   {total_value}")
+    print(f"  Used capacity: {used_capacity}/{capacity}")
+    print("\nItems in knapsack:")
+    for item in items_in_knapsack:
+        print(f"  {item}")
+
+    pass
+
+
+
---
+++ Aufgabenblatt6/util.py
@@ -0,0 +1,104 @@
+
+
+class Item:
+
+    def __init__(self, id, weight, value):
+        """
+        Initialize Item object
+        """
+        self.id = id
+        self.weight = weight
+        self.value = value
+        self.profitability = value/weight
+
+    def __str__(self):
+        return f"Item {self.id}: value = {self.value}, weight = {self.weight}, profitability = {self.profitability}."
+
+
+def read_instance_from_file(path, print_instance=False):
+    """
+    Read an instance from a file
+    :param path: Path to the instance file
+    :param print_instance: If true, information about instance will be printed
+    :return: num_items, weights, values, capacity
+    """
+    filename = path.replace('\\', '/').split('/')[-1]
+    print(f"Reading instance: {filename}")
+    # read file
+    with open(path, 'r') as file:
+        for line_number, line in enumerate(file):
+            line = line.replace("\n", "").split(" ")
+            if line_number == 0:
+                # get number of items from first line
+                num_items = int(line[0])
+            elif line_number == 1:
+                # get item weights from second line
+                weights = list(map(int, line))
+            elif line_number == 2:
+                # get item values from third line
+                values = list(map(int, line))
+            elif line_number == 3:
+                # get capacity from fourth line
+                capacity = int(line[0])
+
+    # print instance
+    if print_instance:
+        print(f"  Number of items: {num_items}")
+        print(f"  Item weights:    {weights}")
+        print(f"  Item values:     {values}")
+        print(f"  Capacity:        {capacity}")
+
+    return num_items, weights, values, capacity
+
+
+def create_item_classes(weights, values):
+    """
+    Create item objects using the Item class
+    :param weights: list of item weights
+    :param values: list of item values
+    :return: List of all created item objects
+    """
+
+    if len(weights) != len(values):
+        raise ValueError("Number of weights and values given is not equal. Can't create classes.")
+
+    items = []
+    for i, (weight, value) in enumerate(zip(weights, values)):
+        items.append(Item(i, weight, value))
+
+    return items
+
+
+def total_value_of_items(items):
+    """
+    Calculated total value of item objects in a given list
+    :param items: list of item objects
+    :return: total value
+    """
+    total_value = 0
+    for item in items:
+        total_value += item.value
+    return total_value
+
+
+def total_weight_of_items(items):
+    """
+    Calculated total weight of item objects in a given list
+    :param items: list of item objects
+    :return: total weight
+    """
+    total_weight = 0
+    for item in items:
+        total_weight += item.weight
+    return total_weight
+
+
+if __name__ == "__main__":
+    """
+    This code is only used for testing
+    """
+
+    num_items, weights, values, capacity = read_instance_from_file("./instances/whoopdeedoo.txt", print_instance=True)
+    items = create_item_classes(weights, values)
+
+    pass
